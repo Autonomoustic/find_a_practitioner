6254d08e1edc68db5ecaaf77df5e3b41518668db
Ali Aziz <aliaziz@Alis-MacBook-Pro.local>
2018-08-20 15:19:33 +0100

    made appoiointments controllers, updated some table info

--- db/migrate/20180820132054_create_paitents.rb
+++ db/migrate/20180820132054_create_paitents.rb
@@ -1,6 +1,6 @@
-class CreatePaitents < ActiveRecord::Migration[5.2]
+class CreatePatients < ActiveRecord::Migration[5.2]
   def change
-    create_table :paitents do |t|
+    create_table :patients do |t|
       t.string :name
       t.integer :age
       t.string :address
