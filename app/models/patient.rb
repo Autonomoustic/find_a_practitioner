545cfa282a9e60fabc0da679e4f43d9f14409ef8
github username <github email address>
2018-08-21 10:07:06 +0100

    practitioner and patient controller and views

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,15 +1,15 @@
 class Patient < ApplicationRecord
   has_many :appointments
   has_many :practitioners, through: :appointments
-  # belongs_to :clinic
+
 
   validates :name, presence: true
-  validates :age, presence: true, numericality: { greater_than_or_equal_to :18}
+  validates :age, presence: true, numericality: { greater_than_or_equal_to: 18}
   validates :address, presence: true
-  validates :medical_hisotry, length: (maximum: 300)
+  validates :medical_history, length: {maximum: 300}
 
 
-#it makes 
+#it makes it possible for a patient to go to more than one clinic
   def clinics
     self.practitioners.map do |practitioner|
       practitioner.department.clinic
